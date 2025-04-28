b888e832c1f92fe9047827ee437c6e3319306ce5
hiroto <6hiroto9@gmail.com>
2025-04-28 23:21:25 +0900

    画面実装

---
+++ 002_Caluclator/src/main/java/com/example/calculator/controller/CalcController.java
@@ -0,0 +1,37 @@
+package com.example.calculator.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalcController {
+	
+	@GetMapping("/top")
+	public String showCalc(Model model) {
+		
+		// 数字表示項目
+		List<Integer> numberList = new ArrayList<>();
+		for(int i = 9; i >= 0; i--) {
+			numberList.add(i);
+		}
+		
+		// 記号表示項目
+		List<String> markList = new ArrayList<>();
+		markList.add("C");
+		markList.add("＋");
+		markList.add("－");
+		markList.add("×");
+		markList.add("÷");
+		markList.add("＝");
+		markList.add(".");
+		
+		model.addAttribute("numberList", numberList);
+		model.addAttribute("markList", markList);
+		
+		return "show-calc";
+	}
+}
