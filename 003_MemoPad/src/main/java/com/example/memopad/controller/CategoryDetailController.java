3dd969e70e63e1278a45f6d450d5941ec52343c7
hiroto <6hiroto9@gmail.com>
2025-05-10 19:56:55 +0900

    登録処理のリクエストパスとパラメータの修正、戻るボタンの修正

--- 003_MemoPad/src/main/java/com/example/memopad/controller/CategoryDetailController.java
+++ 003_MemoPad/src/main/java/com/example/memopad/controller/CategoryDetailController.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.memopad.entity.Category;
 import com.example.memopad.entity.CategoryDetail;
 import com.example.memopad.entity.Status;
 import com.example.memopad.form.CategoryDetailForm;
@@ -61,6 +65,60 @@ public class CategoryDetailController {
 		return "category-detail";
 	}
 	
+	// 確認画面リクエスト(登録)
+	@PostMapping(value="/confirm-category-detail", params="regist")
+	public String confirmCategoryDetailRegist(
+			@Validated @ModelAttribute CategoryDetailForm form,
+			BindingResult result,
+			Model model) {
+
+		form.setModeFlg(REGIST);
+		
+		if(result.hasErrors()) {
+			
+			List<Status> statusList = statusService.findAll();
+			model.addAttribute("statusList", statusList);
+			
+			return "category-detail";
+		}
+		
+		Status status = statusService.findById(form.getStatusId());
+		form.setStatusName(status.getStatusName());
+		
+		return "category-detail-confirm";
+	}
+	
+	// 処理リクエスト(登録)
+	@PostMapping(value="/category-detail", params="regist")
+	public String categoryDetailRegist(
+			@Validated @ModelAttribute CategoryDetailForm form,
+			BindingResult result,
+			RedirectAttributes redirectAttributes,
+			Model model) {
+		
+		if(result.hasErrors()) {
+			
+			form.setModeFlg(REGIST);
+			
+			List<Status> statusList = statusService.findAll();
+			model.addAttribute("statusList", statusList);
+			
+			return "category-detail";
+		}
+		
+		Category category = new Category();
+		category.setCategoryName(form.getCategoryName());
+		category.setStatusId(form.getStatusId());
+		category.setRemarks(form.getRemarks());
+		
+		// 登録処理
+		categoryService.regist(category);
+		
+		redirectAttributes.addFlashAttribute("msg", "カテゴリー登録完了");
+		
+		return "redirect:/category-complete";
+	}
+	
 	// 画面表示リクエスト(更新)
 	@PostMapping(value="show-category-detail", params="update")
 	public String showCategoryDetailUpdate(
@@ -103,55 +161,4 @@ public class CategoryDetailController {
 		
 		return "category-detail";
 	}
-	
-	
-//	// 登録確認画面リクエスト
-//	@PostMapping("/confirm-category-regist")
-//	public String confirmCategoryRegist(
-//			@Validated @ModelAttribute CategoryRegistForm form,
-//			BindingResult result,
-//			Model model) {
-//		
-//		if(result.hasErrors()) {
-//			
-//			List<Status> statusList = statusService.findAll();
-//			model.addAttribute("statusList", statusList);
-//			
-//			return "category-regist";
-//		}
-//		
-//		Status status = statusService.findById(form.getStatusId());
-//		form.setStatusName(status.getStatusName());
-//		
-//		return "confirm-category-regist";
-//	}
-//	
-//	// 登録処理リクエスト
-//	@PostMapping("/category-regist")
-//	public String categoryRegist(
-//			@Validated @ModelAttribute CategoryRegistForm form,
-//			BindingResult result,
-//			RedirectAttributes redirectAttributes,
-//			Model model) {
-//		
-//		if(result.hasErrors()) {
-//			
-//			List<Status> statusList = statusService.findAll();
-//			model.addAttribute("statusList", statusList);
-//			
-//			return "category-regist";
-//		}
-//		
-//		Category category = new Category();
-//		category.setCategoryName(form.getCategoryName());
-//		category.setStatusId(form.getStatusId());
-//		category.setRemarks(form.getRemarks());
-//		
-//		// 登録処理
-//		categoryService.regist(category);
-//		
-//		redirectAttributes.addFlashAttribute("msg", "カテゴリー登録完了");
-//		
-//		return "redirect:/category-complete";
-//	}
 }
