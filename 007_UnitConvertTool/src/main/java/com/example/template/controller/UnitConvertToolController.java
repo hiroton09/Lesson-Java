e909f10b02e97ef1398c1e2b2c100d14e1b8d082
hiroto <6hiroto9@gmail.com>
2025-06-01 07:37:17 +0900

    画面スタイル作成

--- 007_UnitConvertTool/src/main/java/com/example/template/controller/UnitConvertToolController.java
+++ 007_UnitConvertTool/src/main/java/com/example/template/controller/UnitConvertToolController.java
@@ -1,13 +1,20 @@
 package com.example.template.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.template.entity.UnitSummaryEntity;
 
 @Controller
 public class UnitConvertToolController {
 
 	@GetMapping("/top")
-	public String init() {
+	public String init(Model model) {
+		
+		UnitSummaryEntity unitSummary = new UnitSummaryEntity();
+		model.addAttribute("unitSummary", unitSummary);
+		
 		return "index";
 	}
 }
